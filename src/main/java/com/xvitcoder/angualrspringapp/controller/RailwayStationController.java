3f282f8cf08ce4ab0e48b6d100473dedb8575913
Bayo Erinle <berinle@gmail.com>
2015-01-12 10:36:38 -0500

    moved to spring boot + gradle combo

--- src/main/java/com/xvitcoder/angualrspringapp/controller/RailwayStationController.java
+++ src/main/java/com/xvitcoder/angualrspringapp/controller/RailwayStationController.java
@@ -1,7 +1,7 @@
 package com.xvitcoder.angualrspringapp.controller;
 
-import java.util.List;
-
+import com.xvitcoder.angualrspringapp.beans.RailwayStation;
+import com.xvitcoder.angualrspringapp.service.RailwayStationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.xvitcoder.angualrspringapp.beans.RailwayStation;
-import com.xvitcoder.angualrspringapp.service.RailwayStationServiceImpl;
+import java.util.List;
 
 @Controller
 @RequestMapping("/railwaystations")
